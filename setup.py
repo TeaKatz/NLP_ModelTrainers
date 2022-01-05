cc212f02b2c9204585ab172cef0101b5882edf92
Panuthep <falcon_270394@hotmail.co.th>
2022-01-05 20:43:31 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
-import os
-
 from glob import glob
 from os.path import basename, splitext
 from setuptools import find_packages, setup
 
-
-base_path = os.path.dirname(__file__) + "/.."
-nlp_losses_path = base_path + "/NLP_Losses/"
-nlp_metrics_path = base_path + "/NLP_Metrics/"
 
 setup(
     name="nlp_modeltrainers",
@@ -17,8 +11,4 @@ setup(
     py_modules=[splitext(basename(path))[0] for path in glob("src/nlp_modeltrainers/*.py")] + \
             [splitext(basename(path))[0] for path in glob("src/nlp_modeltrainers/sentence_classification/*.py")] + \
             [splitext(basename(path))[0] for path in glob("src/nlp_modeltrainers/word_embedding/*.py")],
-    install_requires=[
-        "nlp_losses @ file://localhost/%s" % nlp_losses_path,
-        "nlp_metrics @ file://localhost/%s" % nlp_metrics_path,
-        ],
 )
--- src/nlp_modeltrainers/glue_benchmark/STSBTrainerModule.py
+++ src/nlp_modeltrainers/glue_benchmark/STSBTrainerModule.py
@@ -3,23 +3,15 @@ from nlp_losses import Losses
 from nlp_metrics.glue_benchmark import STSBMetric
 from ..BaseTrainerModule import BaseTrainerModule
 
-N_CLASSES = 1
-
 
 class STSBTrainerModule(BaseTrainerModule):
-    def __init__(self, model, hidden_size, **kwargs):
+    def __init__(self, model, **kwargs):
         super().__init__(**kwargs)
         self.model = model
-        for param in self.model.parameters():
-            param.requires_grad = False
-        self.model.eval()
-        self.classifier = torch.nn.Linear(3 * hidden_size, N_CLASSES)
 
     def forward(self, *args, **kwargs):
-        with torch.no_grad():
-            outputs = self.model(*args, **kwargs)
-        outputs = torch.cat((outputs[:, 0], outputs[:, 1], torch.abs(outputs[:, 0] - outputs[:, 1])), dim=-1)
-        outputs = self.classifier(outputs)
+        outputs = self.model(*args, **kwargs)
+        outputs = torch.nn.CosineSimilarity(dim=1)(outputs[:, 0], outputs[:, 1])
         return outputs
 
     @staticmethod
@@ -32,13 +24,11 @@ class STSBTrainerModule(BaseTrainerModule):
 
     def cal_loss(self, outputs, targets):
         """
-        outputs: (batch_size, n_classes)
+        outputs: (batch_size, )
         targets: (batch_size, )
         """
         outputs = outputs.float()
         targets = targets.float()
-        # (batch_size, )
-        outputs = outputs.squeeze(1)
         return self.loss_func(outputs, targets)
 
     def cal_metrics(self, outputs, targets):
@@ -51,6 +41,4 @@ class STSBTrainerModule(BaseTrainerModule):
 
         outputs = outputs.astype(float)
         targets = targets.astype(float)
-        # (batch_size, )
-        outputs = outputs.squeeze(1)
         return self.metrics_func(outputs, targets)
